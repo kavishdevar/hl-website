69208326982598eff8804d13a0ad3003f82c4b6c
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-01-19 02:33:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const audioCtrl = document.getElementById('audioCtrl');
+    const audioImg = document.getElementById('audioImg');
+    const bgAudio = document.getElementById('bgAudio');
+
+    const audioOnImg = "music.png";
+    const audioOffImg = "https://static.thenounproject.com/png/1680900-200.png";
+
+    audioImg.src = audioOffImg;
+
+    audioCtrl.addEventListener('click', () => {
+      if (bgAudio.paused) {
+        bgAudio.play();
+        audioImg.src = audioOnImg;
+      } else {
+        bgAudio.pause();
+        audioImg.src = audioOffImg;
+      }
+    });
