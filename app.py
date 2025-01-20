388cf7fd09216389137baa1961cdfd4c0a0516f5
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-01-20 13:42:13 +0530

    removed cors as it was giving problems in testing :/

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import requests
 import os 
 from dotenv import load_dotenv
 load_dotenv()   
 app = Flask(__name__)
-CORS(app)  
 
 botToken = os.getenv('BOT_TOKEN')
 channelID = os.getenv('CHANNEL_ID')
